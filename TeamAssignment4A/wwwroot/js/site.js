f4a14a1043bbfa43bfd473868609529492c8a94d
GeorgiadisGeorge <georgiadisgeorge@hotmail.com>
2023-01-26 00:23:23 +0200

    Just a test commit from a new brranch

--- TeamAssignment4A/wwwroot/js/site.js
+++ TeamAssignment4A/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
