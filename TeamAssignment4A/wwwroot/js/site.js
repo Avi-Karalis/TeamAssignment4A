13965df44eaf0debb1f33af238ed30e2b7fb780e
GeorgiadisGeorge <georgiadisgeorge@hotmail.com>
2023-01-26 21:34:59 +0200

    A little closer

--- TeamAssignment4A/wwwroot/js/site.js
+++ TeamAssignment4A/wwwroot/js/site.js
@@ -2,6 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.go-to-top').show();
+        } else {
+            $('.go-to-top').hide();
+        }
+    });
+});
+
 function scrollToTop() {
     window.scrollTo({
         top: 0,
@@ -48,6 +58,8 @@ function type() {
 }
 
 type();
+
+
 
 
 
