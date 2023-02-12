c3f1c4c8fb080168eb3ff5621c560801c43c7c9b
Avi-Karalis <Averkkaralis@gmail.com>
2023-02-13 00:41:45 +0200

    manual grading works

---
+++ TeamAssignment4A/Dtos/ManualGradingExamDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamAssignment4A.Dtos {
+    public class ManualGradingExamDTO {
+
+        public string Question { get; set; }
+        public char SubmittedAnswer { get; set; }
+        public char CorrectAnswer { get; set; }
+        public string Result { get; set; }
+        public int CandidateExamStemId { get; set; }
+    }
+}
